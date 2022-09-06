a96470f6c81825f7354ad3999ce2477af992bebd
Gautam Aggarwal <gaggarwal2810@gmail.com>
2022-09-06 20:29:27 +0530

    Add files via upload

---
+++ constants/assets.js
@@ -0,0 +1,41 @@
+import badge from "../assets/icons/badge.png";
+import eth from "../assets/icons/eth.png";
+import heart from "../assets/icons/heart.png";
+import left from "../assets/icons/left.png";
+import search from "../assets/icons/search.png";
+
+import logo from "../assets/images/logo.png";
+import nft01 from "../assets/images/nft01.jpg";
+import nft02 from "../assets/images/nft02.jpg";
+import nft03 from "../assets/images/nft03.png";
+import nft04 from "../assets/images/nft04.png";
+import nft05 from "../assets/images/nft05.png";
+import nft06 from "../assets/images/nft06.jpeg";
+import nft07 from "../assets/images/nft07.jpeg";
+
+import person01 from "../assets/images/person01.jpg";
+import person02 from "../assets/images/person02.png";
+import person03 from "../assets/images/person03.png";
+import person04 from "../assets/images/person06.png";
+
+export default {
+  badge,
+  eth,
+  heart,
+  left,
+  search,
+
+  logo,
+  nft01,
+  nft02,
+  nft03,
+  nft04,
+  nft05,
+  nft06,
+  nft07,
+
+  person01,
+  person02,
+  person03,
+  person04,
+};
---
+++ constants/dummy.js
@@ -0,0 +1,194 @@
+import assets from "./assets";
+
+const NFTData = [
+  {
+    id: "NFT-01",
+    name: "Pudgy Penguins",
+    creator: "De Villiers",
+    price: 7.35,
+    description:
+      "MEET THE PUDGY PENGUINS TEAM. A TEAM OF BUILDERS, THINKERS, OPERATORS, AND CREATORS, ALL UNIFIED WITH THE PURPOSE OF EXCELLING PUDGY PENGUINS INTO THE FASTEST GROWING WEB3 BRAND IN THE WORLD.WITH THE GOAL OF BUILDING AND INNOVATING, THE BROADER VERSION OF OUR TEAM IS THE HUDDLE, OUR COMMUNITY. WITH OUR COMMUNITY THAT FOSTERS NUMEROUS PERSPECTIVES AND IDEAS, WE ARE ALL WORKING TOWARDS BUILDING A MASS-ADOPTABLE WEB3 BRAND.",
+    image: assets.nft01,
+    bids: [
+      {
+        id: "BID-11",
+        name: "Jessica Tan",
+        price: 7.45,
+        image: assets.person02,
+        date: "December 12, 2021 at 12:10 PM",
+      },
+      {
+        id: "BID-12",
+        name: "Jennifer Sia",
+        price: 7.40,
+        image: assets.person03,
+        date: "December 27, 2021 at 1:50 PM",
+      },
+      {
+        id: "BID-13",
+        name: "Rosie Wong",
+        price: 7.45,
+        image: assets.person04,
+        date: "December 31, 2021 at 3:50 PM",
+      },
+    ],
+  },
+  {
+    id: "NFT-02",
+    name: "A Doodle A Day Keeps The Demons Away",
+    creator: "Xi Yuan",
+    price: 7.25,
+    description:
+      "A Doodle a Day Keeps the Demons Away is an ongoing (capped at 100) series of digital illustrations made by Adam Toksöz that feature graffiti style creatures/characters using bright colors and bold lines. Most of these originate as sharpie drawings before being digitized, vectored, & colored.",
+    image: assets.nft02,
+    bids: [
+      {
+        id: "BID-21",
+        name: "Jessica Tan",
+        price: 7.05,
+        image: assets.person04,
+        date: "December 12, 2021 at 12:10 PM",
+      },
+    ],
+  },
+  {
+    id: "NFT-03",
+    name: "Killabears",
+    creator: "Elizabeth Winget",
+    price: 95.25,
+    description:
+      "KILLABEARS is a collection of 3333 thoughtfully designed, animated and randomly generated NFTs on the Ethereum Blockchain by Mexican artist Memo Angeles. KILLABEARS holders can participate in exclusive events, such as: NFT claims, raffles, giveaways and much, much more. Don't forget, all KILLABEARS are special -- but some are especially special.",
+    image: assets.nft03,
+    bids: [
+      {
+        id: "BID-31",
+        name: "Jessica Tan",
+        price: 95.25,
+        image: assets.person02,
+        date: "December 12, 2021 at 12:10 PM",
+      },
+      {
+        id: "BID-32",
+        name: "Jennifer Sia",
+        price: 95.5,
+        image: assets.person03,
+        date: "December 27, 2021 at 1:50 PM",
+      },
+    ],
+  },
+  {
+    id: "NFT-04",
+    name: "FANZONE.io",
+    creator: "Del Potro",
+    price: 54.25,
+    description:
+      "The DFB Road to World Cup is a collection of 25 NFTs — each player and the head coach of the German National Football Team will have one unique 1of1 digital trading card living on the blockchain. The digital cards will be released in 3 drops.",
+    image: assets.nft04,
+    bids: [
+      {
+        id: "BID-41",
+        name: "Jessica Tan",
+        price: 56.25,
+        image: assets.person02,
+        date: "December 12, 2021 at 12:10 PM",
+      },
+      {
+        id: "BID-42",
+        name: "Jennifer Sia",
+        price: 54.25,
+        image: assets.person03,
+        date: "December 27, 2021 at 1:50 PM",
+      },
+      {
+        id: "BID-43",
+        name: "Rosie Wong",
+        price: 55.15,
+        image: assets.person04,
+        date: "December 31, 2021 at 3:50 PM",
+      },
+      {
+        id: "BID-44",
+        name: "Vincent Swift",
+        price: 54.15,
+        image: assets.person02,
+        date: "December 31, 2021 at 3:50 PM",
+      },
+    ],
+  },
+  {
+    id: "NFT-05",
+    name: "Meta Bounty Hunters",
+    creator: "David Morgan",
+    price: 10.25,
+    description:
+      "Meta Bounty Hunters is a digital collection of 8,888 unique NFTs. Meta Bounty Hunters is a first of its kind invite-only community and collective of elite members who share a community-first mindset and are focused on giving back to its NFT holders through the Reflection Rewards.",
+    image: assets.nft05,
+    bids: [
+      {
+        id: "BID-51",
+        name: "Jessica Tan",
+        price: 10.25,
+        image: assets.person02,
+        date: "December 12, 2021 at 12:10 PM",
+      },
+    ],
+  },
+  {
+    id: "NFT-06",
+    name: "Black box model",
+    creator: "Leo Markson",
+    price: 20.25,
+    description:
+      "These boxes contain the data needed to understand the past, present, and future of RENGA Factory. Each box is a portal to a random archetype from worlds unknown. ",
+    image: assets.nft06,
+    bids: [
+      {
+        id: "BID-61",
+        name: "Jessica Tan",
+        price: 20.25,
+        image: assets.person02,
+        date: "December 12, 2021 at 12:10 PM",
+      },
+      {
+        id: "BID-62",
+        name: "Jennifer Sia",
+        price: 20.5,
+        image: assets.person03,
+        date: "December 27, 2021 at 1:50 PM",
+      },
+      {
+        id: "BID-63",
+        name: "Rosie Wong",
+        price: 20.75,
+        image: assets.person04,
+        date: "December 31, 2021 at 3:50 PM",
+      },
+      {
+        id: "BID-64",
+        name: "Siti Nurhaliza",
+        price: 21.25,
+        image: assets.person02,
+        date: "December 31, 2021 at 3:50 PM",
+      },
+      {
+        id: "BID-65",
+        name: "Kaitlyn Lee",
+        price: 7.25,
+        image: assets.person02,
+        date: "December 31, 2021 at 3:50 PM",
+      },
+    ],
+  },
+  {
+    id: "NFT-07",
+    name: "Abstracto soulful art",
+    creator: "Victor de la Cruz",
+    price: 18.25,
+    description:
+      "Soulful and spiritual paintings & drawings.",
+    image: assets.nft07,
+    bids: [],
+  },
+];
+
+export { NFTData };
